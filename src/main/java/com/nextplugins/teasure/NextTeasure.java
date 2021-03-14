bd05e765b097b1e51d3db80c0f30331f6b840380
Yuhtin <rio.daviduarte@gmail.com>
2021-03-14 17:41:24 -0300

    Initial commit

---
+++ src/main/java/com/nextplugins/teasure/NextTeasure.java
@@ -0,0 +1,17 @@
+package com.nextplugins.teasure;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NextTeasure extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
